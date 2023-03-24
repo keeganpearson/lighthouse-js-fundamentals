882f52a269144967e1bbfee657242e509cf9ff8b
keeganpearson <keegan.dpearson@gmail.com>
2023-03-24 22:46:23 +0000

    syntax edit

--- final-position.js
+++ final-position.js
@@ -5,7 +5,7 @@ const finalPosition = (moves) => {
   let x = 0;
   let y = 0;
 
-  for(let i=0; i < moves.length; i++){
+  for(let i = 0; i < moves.length; i++){
     if(moves[i] === 'north') {
       y++;
     } else if (moves[i] === 'south') {
