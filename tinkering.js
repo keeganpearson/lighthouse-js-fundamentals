cdf918d79c357b31d4d3b42e40b7e7577f50d498
keeganpearson <keegan.dpearson@gmail.com>
2023-03-28 22:49:47 +0000

    random work space problem solving

--- tinkering.js
+++ tinkering.js
@@ -1,5 +1,26 @@
-function multiply(a, b) {
-  return a*b;
+
+var inventory = [
+  ["gold pieces", 25],
+  ["belt", 4],
+  ["ring", 1],
+  ["shoes", 2]
+];
+
+inventory.push(["pants",4]);
+inventory.pop();
+
+console.log(inventory.length);
+
+
+
+
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
 }
-const result = multiply(2, 4);
-console.log(result);
+
+doubleSum(5, 4);
+
+console.log(sum);
